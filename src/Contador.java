e689cd3e40daecfbf352139f0e5e62b430a90644
Henrique Rodrigues <desenvolvimento@nagix.com.br>
2024-03-07 17:41:17 -0300

    second commit

--- src/Contador.java
+++ src/Contador.java
@@ -1,2 +1,20 @@
 public class Contador {
+    public static void main(String[] args) {
+        try {
+            int numeroInicial = Integer.parseInt(args[0]);
+            int numeroFinal = Integer.parseInt(args[1]);
+
+            if (numeroInicial > numeroFinal) {
+                throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+            }
+
+            for (int i = numeroInicial; i <= numeroFinal; i++) {
+                System.out.println("Imprimindo o número " + i);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Parâmetros inválidos. Digite dois números inteiros.");
+        } catch (ParametrosInvalidosException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
